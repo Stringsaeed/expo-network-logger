63d8a6093b911105ba3ac525fc6899457a3c34ff
Muhammed Ibrahim <stringsaeed@gmail.com>
2025-05-21 01:26:07 +0300

    chore: add semantic release configuration and update dependencies

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer", // determines version bump
+    "@semantic-release/release-notes-generator", // generates changelog content
+    "@semantic-release/changelog", // writes to CHANGELOG.md
+    "@semantic-release/github", // creates GitHub release
+    "@semantic-release/git", // commits changelog and version bump
+  ],
+};
