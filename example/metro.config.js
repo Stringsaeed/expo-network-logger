645f09ffb4c0b4094e2a510962989c47404f75b7
Muhammed Ibrahim <stringsaeed@gmail.com>
2026-04-29 15:32:57 +0400

    feat: rename package and move to npm

--- example/metro.config.js
+++ example/metro.config.js
@@ -19,7 +19,7 @@ config.resolver.nodeModulesPaths = [
 ];
 
 config.resolver.extraNodeModules = {
-  '@stringsaeed/expo-network-logger': '..',
+  'expo-network-logger': '..',
 };
 
 config.watchFolders = [path.resolve(__dirname, '..')];
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = async (env, argv) => {
     {
       ...env,
       babel: {
-        dangerouslyAddModulePathsToTranspile: ['@stringsaeed/expo-network-logger'],
+        dangerouslyAddModulePathsToTranspile: ['expo-network-logger'],
       },
     },
     argv
